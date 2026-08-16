677500e2fcf900f1bd3b4d0cfce0e014bbaad766
Sony AK <sony@sony-ak.com>
2026-08-17 00:18:00 +0700

    gitignore: ignore crabsoup.test.lua

--- crabsoup.test.lua
+++
@@ -1,7 +0,0 @@
--- Temporary test config: HLS output only
-pl = playlist({directory = "./media", shuffle = false, loop = true})
-j = jingles({directory = "./jingles"})
-live = input.harbor({host = "0.0.0.0", port = 8005, mount = "/live", password = "dj"})
-
-output.hls({directory = "/tmp/opencode/hls", segment_seconds = 4, retention = 12},
-           fallback({j, live, pl}))
